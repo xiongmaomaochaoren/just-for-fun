4ec795126b065436a11091eb1724445ff513f940
wangcheng714 <wangcheng714@gmail.com>
2015-12-08 11:49:37 +0800

    提交示例伪代码

--- Wechat/client/util/wxsdk.js
+++ Wechat/client/util/wxsdk.js
@@ -2,7 +2,12 @@
 
 class WXSdk {
     static getWXSdkPromise(){
-        
+
+        return new Promise( (successCb, failCb) => {
+            wx.config(options);
+            wx.ready(successCb);
+            wx.error(failCb);
+        });
     }
     static share(){
         wx.onMenuShareTimeline();
