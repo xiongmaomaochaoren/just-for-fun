9e640fe46d9ed3f8cec7aa55d07b5c25b34365bc
qiangran <qiangran@github.com>
2015-12-11 14:37:25 +0800

    client js code submit

---
+++ Wechat/client/wc_old/wxsdk.js
@@ -0,0 +1,51 @@
+ import WXShareDataConfig from "./wxShareDataConfig";
+
+/**
+    微信分享类
+**/
+class WXSdk {
+    static getWXSdkPromise(url){
+              return new Promise( (resolve, reject) => {
+                    var xhr = new XMLHttpRequest();
+                    xhr.open("GET", url);
+                    xhr.onreadystatechange = function(){
+                        if(xhr.readyState != 4){
+                            return ;
+                        }
+                        if(xhr.status == 200){
+                            try{
+                                let responseJson = JSON.parse(xhr.response);
+                                resolve(responseJson);
+                            }catch(e){
+                                reject(e.message);
+                            }
+                        }else{
+                            reject(xhr.statusText);
+                        }
+                    };
+                    xhr.send();
+            }) 
+           
+    }
+
+     
+    static share(signaturePackage,wxShareData){
+          
+            signaturePackage["debug"] = true;
+            wx.config(signaturePackage);
+            wx.ready(function (){
+                wxShareData = WXShareDataConfig.mergeShareDate(wxShareData);
+
+                wx.onMenuShareAppMessage(wxShareData);
+                wx.onMenuShareTimeline(wxShareData);
+                wx.onMenuShareQQ(wxShareData);
+                wx.onMenuShareWeibo(wxShareData);
+            });
+
+    }
+
+   
+}
+
+export default WXSdk;
+
